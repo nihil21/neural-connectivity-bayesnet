08e700c99e3116850223c00f85bbacbddf413ecb
nihil21 <mattia.orlandi21@gmail.com>
2020-10-07 18:19:43 +0200

    Added heuristics comparison, moved worker.py code to notebook

--- src/worker.py
+++
@@ -1,5 +0,0 @@
-# Define function for evaluating each edge connecting to X_n
-def edge_eval(x_c, x_n, model, estimator):
-    copy = model.copy()
-    copy.add_edge(x_c, x_n)
-    return estimator.score(copy)
